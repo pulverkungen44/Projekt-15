b18f3ce4673a3448b7a6490961066d7b4b5eb0a4
pulverkungen44 <alexander.bosovitch@hotmail.com>
2016-12-16 11:34:48 +0100

    Update Projekt15Jvariables.js

--- Projekt15Jvariables.js
+++ Projekt15Jvariables.js
@@ -1,78 +1,134 @@
-//Variables
-p$auto_dmg = 1;
-e$auto_dmg = 0;
-p$health = 10;
-e$health = 10;
+//Shells and shots
+c37$AP = {b_factor: 6410, muzzle_vel: 792, mass: 0.87, filler: 0,
+pen100: 80, pen200: 75, pen300: 70, pen400: 67, pen500: 63, pen600: 59, pen700: 55, pen800: 52, pen900: 48, pen1000: 45,
+pen1100: 42, pen1200: 40, pen1300: 37, pen1400: 35, pen1500: 33, pen1600: 31, pen1700: 29, pen1800: 27, pen1900: 25, pen2000: 23};
 
+c57$AP = {b_factor: 4711, muzzle_vel: 899, mass: 2.00, filler: 0,
+pen100: 141, pen200: 136, pen300: 129, pen400: 123, pen500: 118, pen600: 112, pen700: 106, pen800: 101, pen900: 97,
+pen1000: 93, pen1100: 89, pen1200: 84, pen1300: 80, pen1400: 76, pen1500: 73, pen1600: 70, pen1700: 67, pen1800: 63, pen1900: 60,
+pen2000: 58};
 
-//Properties
-p$tank = {health: p$health, auto_dmg: p$auto_dmg};
-e$tank = {health: e$health, auto_dmg: e$auto_dmg, number: 1};
+c76$APCBC = {b_factor: 2321, muzzle_vel: 792, mass: 7.00, filler: 65,
+pen100: 127, pen200: 125, pen300: 122, pen400: 119, pen500: 116, pen600: 114, pen700: 111, pen800: 109, pen900: 106,
+pen1000: 104, pen1100: 102, pen1200: 99, pen1300: 97, pen1400: 95, pen1500: 93, pen1600: 91, pen1700: 88, pen1800: 86, pen1900: 84,
+pen2000: 82};
+
+c76$HVAP = {b_factor: 3192, muzzle_vel: 1032, mass: 3.4, core: 1.8,
+pen100: 182, pen200: 178, pen300: 173, pen400: 170, pen500: 166, pen600: 162, pen700: 158, pen800: 154, pen900: 150,
+pen1000: 146, pen1100: 142, pen1200: 139, pen1300: 136, pen1400: 132, pen1500: 129, pen1600: 126, pen1700: 122, pen1800: 119, pen1900: 116,
+pen2000: 113};
+
+//Targets
+t$1 = {
+	//Chassi
+	upperFront_s: 2.5 * 0.8,
+	upperFront_t: 30,
+	lowerFront_s: 2.5 * 0.5,
+	lowerFront_t: 25,
+	side_s: 4.5 * 1.6,
+	side_t: 20,
+	upperRear_s: 2.5 * 0.8,
+	upperRear_t: 20,
+	lowerRear_s: 2.5 * 0.5,
+	lowerRear_t: 15,
+	
+	//Turret
+	turretFront_s: 2.3 * 0.9,
+	turretFront_t: 30,
+	turretSide_s: 2.8 * 1.6,
+	turretSide_t: 20,
+	turretRear_s: 2.2 * 0.9,
+	turretRear_t: 20,
+}
+
+t$2 = {
+	//Chassi
+	upperFront_s: 2.5 * 0.8,
+	upperFront_t: 50,
+	lowerFront_s: 2.5 * 0.5,
+	lowerFront_t: 35,
+	side_s: 4.5 * 1.6,
+	side_t: 30,
+	upperRear_s: 2.5 * 0.8,
+	upperRear_t: 25,
+	lowerRear_s: 2.5 * 0.5,
+	lowerRear_t: 20,
+	
+	//Turret
+	turretFront_s: 2.3 * 0.9,
+	turretFront_t: 55,
+	turretSide_s: 2.8 * 1.6,
+	turretSide_t: 35,
+	turretRear_s: 2.2 * 0.9,
+	turretRear_t: 20,
+}
+
+t$3 = {
+	//Chassi
+	upperFront_s: 2.8 * 1.0,
+	upperFront_t: 80,
+	lowerFront_s: 2.8 * 0.7,
+	lowerFront_t: 50,
+	side_s: 4.8 * 1.8,
+	side_t: 50,
+	upperRear_s: 2.8 * 1.0,
+	upperRear_t: 30,
+	lowerRear_s: 2.8 * 0.7,
+	lowerRear_t: 25,
+	
+	//Turret
+	turretFront_s: 2.3 * 0.9,
+	turretFront_t: 70,
+	turretSide_s: 2.8 * 1.6,
+	turretSide_t: 45,
+	turretRear_s: 2.2 * 0.9,
+	turretRear_t: 30,
+}
+
+t$4 = {
+	//Chassi
+	upperFront_s: 3.0 * 1.2,
+	upperFront_t: 100,
+	lowerFront_s: 3.0 * 0.9,
+	lowerFront_t: 80,
+	side_s: 5.2 * 2.0,
+	side_t: 85,
+	upperRear_s: 3.0 * 1.2,
+	upperRear_t: 70,
+	lowerRear_s: 3.0 * 0.9,
+	lowerRear_t: 50,
+	
+	//Turret
+	turretFront_s: 2.8 * 1.2,
+	turretFront_t: 130,
+	turretSide_s: 3.0 * 1.8,
+	turretSide_t: 90,
+	turretRear_s: 2.7 * 0.9,
+	turretRear_t: 70,
+}
 
 //Resources
 res$money = 100;
 
 //Display
 function display() {
-	document.getElementById("ID_plr_health").innerHTML = "Player health: " + p$tank.health.toFixed(2);
-	document.getElementById("ID_enm_health").innerHTML = "Enemy health: " + e$tank.health.toFixed(2);
 	document.getElementById("ID_money").innerHTML = "Money: " + res$money.toFixed(2);
-	document.getElementById("ID_plr_auto_dmg").innerHTML = "DPS: " + p$tank.auto_dmg.toFixed(2);
-	document.getElementById("enemyTank").innerHTML = "Enemy number " + e$tank.number;
-}
-setTimeout(display, 100);
-setInterval(checkIfDead, 100);
-dmg_interval = setInterval(dmg, 1000);
-
-function checkIfDead() {
-	if (e$tank.health < 1) {
-		e$tank.number += 1;
-		e$tank.health = 10;
-		res$money += 1 + (0.1 * e$tank.number);
-		display();
-	} else {}
+	
 }
 
-function dmg() {
-	e$tank.health -= p$tank.auto_dmg;
-	display();
-}
 
-function upgrade(type, cal) {
-	if (type == "cannon") {
-		if (cal == 37) {
-			var prc = 50;
-				if (res$money >= prc) {
-					p$tank.auto_dmg *= 2;
-					res$money -= prc;
-					display();
-				} 
-		} else if (cal == 40) {
-			var prc = 100;
-				if (res$money >= prc) {
-					p$tank.auto_dmg *= 3;
-					res$money -= prc;
-					display();
-				} 
-		} else {
-			window.alert("You can't afford this upgrade");
-		}
-	} else if (type == "turret") {
-		var prc = 100;
-		if (res$money >= prc) {
-			clearInterval(dmg_interval);
-			dmg_interval = setInterval(dmg, 990);
-			res$money -= prc;
-			prc *= 1.02;
-			display();
-		} else {
-		window.alert("You can't afford this upgrade");
-		}
+
+function penResult(s, t) {
+	if (s.pen600 > t.upperFront_t) {
+		var result = "Penetration";
+	} else {
+		var result = "No penetration";
 	}
-
+	
+	document.getElementById("ID_results").innerHTML = result;
 }
+penResult(c57$AP, t$3);
 
 function show() {
 	document.getElementById("ID_upgradesBox").classList.toggle("box_row_h");
-	document.getElementById("ID_upgradesBox").classList.toggle("box_row");
 }
